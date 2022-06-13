5ee3c4b2e29b1d34380ceed2023fdd5b56047bb5
Jesus Alejandro Martinez Juarez <jesus.martinez@wizeline.com>
2022-06-13 11:31:14 -0500

    test: test signed commits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by SIGNING COMMITS{" "}
+          Get started by Testing SIGNING COMMITS{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
